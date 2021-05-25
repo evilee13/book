da64205bfa720c51daa7239786b5a16b2be3e7aa
Elina <e.gabbasova@bftcom.com>
2021-05-25 15:00:24 +0500

    Дата публикации

--- aboutBook.js
+++ aboutBook.js
@@ -9,18 +9,4 @@ for (let books in params) {
     if (element) {
         element.innerHTML = (params[books]);
     }
-  //  switch (params.title) {
-    //    case 'Мастер и Маргарита':
-      //      document.getElementById('book1').innerHTML="<img src='1.jpg'/>";
-        //    break;
-        //case 'Евгений Онегин':
-          //  document.getElementById('book2').innerHTML="<img src='2.png'/>";
-            //break;
-        //case    'Мцыри':
-          //  document.getElementById('book3').innerHTML="<img src='3.jpg'/>";
-            //break;
-        //case 'Рассказ о Шерлоке Холмсе':
-          //  document.getElementById('book4').innerHTML="<img src='4.jpg'/>";
-            //break;
-    }
-//}
+}

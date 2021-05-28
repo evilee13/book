3c5b4611fcc151ecdbe5ae9e8a11073b5887122a
Elina <e.gabbasova@bftcom.com>
2021-05-28 13:55:20 +0500

    Получение параметров по идентификатору,ссылка без обработки клика

--- aboutBook.js
+++ aboutBook.js
@@ -1,12 +1,42 @@
+import {dataSource} from "./baseOfBooks.js";
+
 "use strict";
-let urlParams = new URLSearchParams(document.location.search);
-let params = {};
-urlParams.forEach((p, key) => {
-    params [key] = p;
-});
-for (let books in params) {
-    let element = document.getElementById(books);
-    if (element) {
-        element.innerHTML = (params[books]);
+
+function getParams() {
+    let parameters = {};
+    document.location.search
+        .substring(1)
+        .split('&')
+        .forEach((item) => {
+            let param = item.split('=');
+            parameters [param [0]] = param [1];
+        });
+    return parameters;
+}
+
+function getElement() {
+    let params = getParams();
+    return dataSource.find(item => {
+        return Number.parseInt(item.id) === Number.parseInt(params.id)
+    });
+}
+
+function fillElement() {
+    let book = getElement();
+    for (let books in book) {
+        let element = document.getElementById(books);
+        if (element) {
+            if (element) {
+                if (books === 'img') {
+                    let img = document.createElement('img')
+                    img.src = book[books]
+                    element.appendChild(img)
+                } else {
+                    element.innerHTML = (book[books]);
+                }
+            }
+        }
     }
 }
+
+fillElement()
